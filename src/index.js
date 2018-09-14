69a56e26612ca546d5bca5dae300cd00064b5c52
Jonathan Haines <jonno.haines@gmail.com>
2018-09-14 19:49:07 +1000

    done!

--- src/index.js
+++ src/index.js
@@ -6,13 +6,37 @@ import './css/main.css'; // eslint-disable-line import/no-unassigned-import
 
 import Instafeed from 'instafeed.js';
 
-const large = window.matchMedia('(min-width: 768px)');
+const instafeedElement = document.querySelector('.instafeed');
 
-new Instafeed({
-	get: 'user',
-	limit: large.matches ? 16 : 8,
-	userId: '174349777',
-	resolution: 'low_resolution',
-	accessToken: '174349777.1677ed0.d38d143ea61c4cc1a4562d62a3e41960',
-	target: document.querySelector('.instafeed')
-}).run();
+if ('IntersectionObserver' in window) {
+	const observer = new IntersectionObserver(
+		entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					createInstafeed(entry.target).run();
+					observer.disconnect();
+				}
+			});
+		},
+		{
+			threshold: 0
+		}
+	);
+
+	observer.observe(instafeedElement);
+} else {
+	createInstafeed(instafeedElement).run();
+}
+
+function createInstafeed(target) {
+	const large = window.matchMedia('(min-width: 768px)');
+
+	return new Instafeed({
+		get: 'user',
+		limit: large.matches ? 16 : 8,
+		userId: '174349777',
+		resolution: 'low_resolution',
+		accessToken: '174349777.1677ed0.d38d143ea61c4cc1a4562d62a3e41960',
+		target
+	});
+}
